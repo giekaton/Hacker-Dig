0f69ecd8398d129b7313a9cc2ed960f786a73c29
dziungles <info@efektyvusdizainas.lt>
2018-11-04 01:51:38 -0600

    Update

--- setup/config.php
+++ setup/config.php
@@ -1,5 +1,26 @@
 <?php 
 
+//
+// REQUIRED CONFIGURATION
+//
+
+// Database config
+$servername = ""; // UPDATE THIS
+$username = ""; // UPDATE THIS
+$password = ""; // UPDATE THIS
+$dbname = ""; // UPDATE THIS
+
+// Create db connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check db connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//
+// OPTIONAL CONFIGURATION
+//
+
 // PHPMailer configuration
 require_once 'class-phpmailer.php';
 require_once 'class-smtp.php';
@@ -14,30 +35,13 @@ $mail->Username = ''; // UPDATE THIS
 $mail->Password = ''; // UPDATE THIS
 $mail->setFrom('noreply@hackerdig.com', 'Hacker Dig'); // UPDATE THIS
 
-
 // Admin email
 $GLOBALS['adminaddress'] = ''; // UPDATE THIS
 
-
-// Database config
-$servername = ""; // UPDATE THIS
-$username = ""; // UPDATE THIS
-$password = ""; // UPDATE THIS
-$dbname = ""; // UPDATE THIS
-
-
-// Create db connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check db connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-
-// Aylien API config
+// Aylien API config for summary extraction
+// Can get a free api key at https://developer.aylien.com/signup
 $GLOBALS['appKey'] = ""; // UPDATE THIS
 $GLOBALS['appId'] = ""; // UPDATE THIS
-
 
 // Google RECAPTCHA config
 $GLOBALS['reCaptchaSecret'] = ""; // UPDATE THIS
